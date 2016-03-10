ae5749fabbe0a8ce26930637d8ae360214caf933
Nicholas Johnson <nicholas@forwardadvance.com>
2016-03-10 11:19:12 +0000

    exercise

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -9,65 +9,7 @@
 angular.module('app', [])
   .controller('catController', function($scope, $http) {
 
-    $scope.search = {}
-
-    $scope.thundercats = [
-      {
-        name: "Cheetara",
-        specialSkill: "speed",
-        planet: "Thundera",
-        image: "images/cheetara.jpg"
-      },
-      {
-        name: "Lion-O",
-        specialSkill: "Sword of Omens",
-        planet: "Thundera",
-        image: "images/lion-o.jpg"
-      },
-      {
-        name: "Panthro",
-        specialSkill: "Mechanical Engineering",
-        planet: "Thundera",
-        image: "images/panthro.jpg"
-      },
-      {
-        name: "Jaga the Wise",
-        specialSkill: "Wisdom",
-        planet: "Thundera",
-        image: "images/jaga.jpg"
-      },
-      {
-        name: "Snarf",
-        specialSkill: "Annoyance",
-        planet: "Thundera",
-        image: "images/snarf.jpg"
-      },
-      {
-        name: "Robear Berbil",
-        specialSkill: "Farming",
-        planet: "Ro-Bear",
-        image: "images/berbils.jpg"
-      }
-    ];
-
   })
-
-
-
-
-// $scope.inspectScope = function() {
-//   $scope;
-// }
-
-
-
-
-
-
-
-
-
-
 
 
 // $scope.thundercats = [
